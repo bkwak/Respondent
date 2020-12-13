fd0d8a8df05b75709ca02d48184d7abcc951fd78
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-12-13 03:49:04 -0800

    algorithm finished

--- index.js
+++ index.js
@@ -1,71 +1,62 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
-const winners = new Array(8);
+const radians = (deg) => {
+    return (deg * Math.PI) / 180;
+}
+
+const winners = [];
 // name, distance, matching score
 
 const {
     cities, 
     professionalJobTitles: jobTitles, 
-    professionalIndustry: industry, 
+    professionalIndustry: industries, 
     education
 } = JSON.parse(fs.readFileSync('./project.json'));
 
-const coordinates = cities.map(city => city.location.location);
-console.log(coordinates);
-
-
-// [
-//     'Developer',
-//     'Software Engineer',
-//     'Software Developer',
-//     'Programmer',
-//     'Java Developer',
-//     'Java/J2EE Developer',
-//     'Java Full Stack Developer',
-//     'Java Software Engineer',
-//     'Java Software Developer',
-//     'Application Architect',
-//     'Application Developer'
-//   ]
-
-
-
+const coordinates = cities.map(city => {
+    return {
+        lat: radians(city.location.location.latitude),
+        long: radians(city.location.location.latitude)
+    }
+});
 
 fs.createReadStream('./respondents_data_test.csv')
   .pipe(csv())
   .on('data', (row) => {
     // console.log(row);
-    // generateScores(row);
+    generateScores(row);
   })
   .on('end', () => {
-    console.log('CSV file successfully processed');
+    winners.sort((a,b) => b.points - a.points);
+    console.log(winners);
   });
 
 const generateScores = (respondent) => {
     if(!jobTitles.includes(respondent.jobTitle)) return;
     let points = 0;
-    if(respondent.industry) {
-        points += respondent.industry.split(',').filter(category => jobTitles.includes(category)).length;
-    }
-    points += minDistance(respondent.latitude, respondent.longitude);
+    if(respondent.industry) points += respondent.industry.split(',').filter(category => industries.includes(category)).length*10;
+    //1 point for every m closest to location
+    const distance = minDistance(respondent.latitude, respondent.longitude);
+    // console.log(typeof distance);
+    points += distance;
+    console.log(typeof distance, typeof points)
+    winners.push({name: respondent.firstName, points, distance});
 
+};
 
-    // \Delta \sigma =\arccos {\bigl (}\sin \phi _{1}\sin \phi _{2}+\cos \phi _{1}\cos \phi _{2}\cos(\Delta \lambda ){\bigr )}.}
 
 const minDistance = (lat, long) => {
-    const radius = 6357000;
+    const radius = 6357;
 
-    let distance = 0;
-    coordinates.forEach( city => distance = Math.min(distance, calculateDistance(city.latitude, city.longitude, lat, long)));
-    return distance;
+    let distance = Infinity;
+    coordinates.forEach( city => distance = Math.min(distance, calculateDistance(city.lat, city.long, radians(lat), radians(long))/1000));
+    return distance > 100 ? 0 : +distance.toFixed(2);
 
     function calculateDistance(cityLat, cityLong, lat, long) {
-        const centralAngle = 
-        return radius * centralAngle 
+        const centralAngle = Math.acos(Math.sin(cityLat)*Math.sin(lat) + Math.cos(cityLat)*Math.cos(lat)*Math.cos(long - cityLong));
+        return radius * centralAngle; 
     }
+};
 
-
-}
-
-}
